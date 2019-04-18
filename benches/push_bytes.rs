b1e4b6c93fcab1618a1788a5b3c2d36f0cd5f15c
Ahmed Mostafa <amk67482@gmail.com>
2019-04-18 23:22:49 +0200

    Some bug fixes and improvements
    - using `b""` for better reading, no performance lose.
    - Converting usize now to a real ascii byte array.

--- benches/push_bytes.rs
+++ benches/push_bytes.rs
@@ -9,6 +9,12 @@ mod tests {
     use test::Bencher;
 
     #[bench]
+    fn bench_push_bstr_to_buffer(b: &mut Bencher) {
+        let mut buffer = BytesMut::new();
+        b.iter(|| push(&mut buffer, b"HTTP/1.1 "));
+    }
+
+    #[bench]
     fn bench_push_to_buffer(b: &mut Bencher) {
         let mut buffer = BytesMut::new();
 
@@ -45,13 +51,13 @@ mod tests {
         let mut buffer = BytesMut::new();
 
         b.iter(|| {
-            let mut length = "HTTP/1.1 200 Ok".len() as u16;
             let mut data: [u8; 4] = [0; 4];
+            let mut length = "HTTP/1.1 200 Ok".len() as u16;
 
             for i in 1..5 {
-                let base = (10 * i) as u16;
-                data[4 - 1] = (48 + (length % (10 * i) as u16)) as u8;
-                length = &length / (10 * i) as u16;
+                let base = (10u16.pow(4 - (i as u32))) as u16;
+                data[i - 1] = (48 + (length / (10 * i) as u16)) as u8;
+                length = (&length % &base) as u16;
             }
 
             push(&mut buffer, &data);
