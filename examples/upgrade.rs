6e3e0f0c92ede7b12bf8dfad2bbac84b28f87e4c
Ahmed Mostafa <amk67482@gmail.com>
2019-05-06 03:28:47 +0200

    fix: Add support for protocol upgrade on the application layer.

--- examples/upgrade.rs
+++ examples/upgrade.rs
@@ -1,14 +1,16 @@
 extern crate bytes;
 extern crate futures;
+extern crate silver_rs;
 extern crate http;
 extern crate pretty_env_logger;
-extern crate silver_rs;
 extern crate tokio_io;
 
+use silver_rs::app::App;
+use silver_rs::context::Context;
+use silver_rs::error::Error;
 use silver_rs::router::{Route, RouterContext};
 use silver_rs::transport::Io;
 use silver_rs::upgrade::UpgradeContext;
-use silver_rs::{App, Context, Error};
 
 use bytes::Bytes;
 use futures::prelude::*;
@@ -36,9 +38,10 @@ fn upgrade(_cx: &Context, _rcx: &mut RouterContext) -> Result<UpgradeContext, Er
     )
 }
 
-fn main() -> silver_rs::app::Result<()> {
+fn main() -> silver_rs::rt::Result<()> {
     pretty_env_logger::init();
     App::builder()
         .mount(vec![Route::new("/", Method::GET, upgrade)])
+        .finish()?
         .serve()
 }
